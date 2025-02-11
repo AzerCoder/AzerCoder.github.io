e23b0ab71a7d910ff63a4b45c31eeddbf1e4a8b3
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2025-02-11 17:19:51 +0500

    New Version

---
+++ app/api/data/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  return NextResponse.json({
+    success: true,
+    message: 'hle!',
+    data: {
+      message: 'Message and email sent successfully!',
+    }
+  }, { status: 200 });
+};
+
+export const dynamic = 'force-static'
---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'azamjonabdumuxtorov1220@gmail.com',
+    phone: '+998939989980',
+    address: 'Tashkent, Uzbekistan',
+    github: 'https://github.com/AzerCoder',
+    facebook: 'https://www.facebook.com/profile.php?id=100087191440460',
+    linkedIn: 'https://www.linkedin.com/in/azamjonabdumuxtorov/',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: "AzerCoder"
+}
